076ecd516388e6f1ecc9447a45d66f87f7a2acdc
Luis Rodríguez <118550939+luigi1501@users.noreply.github.com>
2026-04-26 01:53:20 -0400

    Actualizar run.py

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
-import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
